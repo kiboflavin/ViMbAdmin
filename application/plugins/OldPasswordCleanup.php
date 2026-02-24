04ec45763e0af0b67d30e9d6f915696f00c0f73d
Paul Haggart <ph@corp.storm.ca>
2026-02-24 12:13:04 -0500

    Add old_password cleanup on mailbox deactivation via toggleActive hook

--- application/plugins/OldPasswordCleanup.php
+++ application/plugins/OldPasswordCleanup.php
@@ -22,6 +22,32 @@ class ViMbAdminPlugin_OldPasswordCleanup extends ViMbAdmin_Plugin implements OSS
      */
     public function mailbox_password_postFlush( $controller, $params )
     {
+        $this->_deleteOldPasswordEntries( $controller );
+    }
+
+    /**
+     * Deletes old password entries after a mailbox is made inactive
+     *
+     * @param object $controller an OSS_Controller_Action instance
+     * @param array $params Additional parameters containing 'active' status
+     * @return void
+     */
+    public function mailbox_toggleActive_postflush( $controller, $params )
+    {
+        if( isset( $params['active'] ) && !$params['active'] )
+        {
+            $this->_deleteOldPasswordEntries( $controller );
+        }
+    }
+
+    /**
+     * Helper method to delete old_password entries for the current mailbox
+     *
+     * @param object $controller an OSS_Controller_Action instance
+     * @return void
+     */
+    private function _deleteOldPasswordEntries( $controller )
+    {
         $controller->getD2EM()->getConnection()->executeStatement(
             'DELETE FROM old_password WHERE username = ?',
             [ $controller->getMailbox()->getUsername() ]
