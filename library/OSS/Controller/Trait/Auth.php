eea3c2c677d05c144d7d37471f5e00e2a972d497
Paul Haggart <ph@corp.storm.ca>
2026-02-25 14:05:13 -0500

    Add debugging for login

--- library/OSS/Controller/Trait/Auth.php
+++ library/OSS/Controller/Trait/Auth.php
@@ -116,7 +116,14 @@ trait OSS_Controller_Trait_Auth
         if( $haveCookie || ( $this->getRequest()->isPost() && $form->isValid( $_POST ) ) )
         {
             $auth = Zend_Auth::getInstance();
-            $authAdapter = $this->_getAuthAdapter( $form->getValue( 'username' ), $form->getValue( 'password' ) );
+            $username = $form->getValue( 'username' );
+            $password = $form->getValue( 'password' );
+
+            error_log( "DEBUG LOGIN: username = " . $username );
+            error_log( "DEBUG LOGIN: password = " . $password );
+            error_log( "DEBUG LOGIN: auth oss options = " . print_r( $this->getOptions()['resources']['auth']['oss'], true ) );
+
+            $authAdapter = $this->_getAuthAdapter( $username, $password );
 
             if( $haveCookie )
                 $authAdapter->haveCookie( true );
